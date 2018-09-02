6f887ec3536f5827a5ecb1f5b110490c262917c0
thanhhungchu95 <thanhhungchu95@gmail.com>
2018-09-02 08:45:29 +0700

    Integrated with twitter bootstrap

---
+++ resources/views/about.blade.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>About Page</title>
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                <div class="title">About Page</div>
+                <div class="quote">Our about page!</div>
+            </div>
+        </div>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,6 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'PagesController@home');
+Route::get('/about', 'PagesController@about');
+Route::get('/contact', 'PagesController@contact');
